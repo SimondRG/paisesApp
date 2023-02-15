afad914af69b0233479a4fbcc11d79a1404f14c3
Simon Rojas <sdavid.rojas@hotmail.com>
2023-02-14 22:15:58 -0500

    Se ajusto el manejo de errores de la petición http 404, se ajusto el tipado de la respuesta http y modifico el tempaple de por-pais pare que llene a tabla de los resultados

--- src/app/pais/pages/por-pais/por-pais.component.ts
+++ src/app/pais/pages/por-pais/por-pais.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { PaisService } from '../../services/pais.service';
+import { Country } from '../../interfaces/pais-interface';
 
 @Component({
   selector: 'app-por-pais',
@@ -7,15 +8,24 @@ import { PaisService } from '../../services/pais.service';
 })
 export class PorPaisComponent {
 
-  termino: string = '';
+  termino  : string = '';
+  hayError : boolean = false; 
+  paises   : Country[] = [];
 
   constructor( private paisService: PaisService ) {}
 
   buscar() {
+
+    this.hayError = false;
     
     this.paisService.buscarPais( this.termino )
-      .subscribe( resp => {
-        console.log( resp );
+      .subscribe( paises => {
+        console.log( paises ); 
+        this.paises = paises;       
+      }, err => {
+        console.log( err );
+        this.hayError = true;
+        this.paises = [];
       });
 
   }
